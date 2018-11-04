53abd10c300b0e7cc087829c1b2e85581363d751
E. Adrián Garro S <adriangarro@icloud.com>
2018-11-03 19:15:28 -0600

    Problem 2 Ready

--- Problems 1-2/vasm_to_vbin.cpp
+++ Problems 1-2/vasm_to_vbin.cpp
@@ -3,6 +3,7 @@
 //  Problem #2: Vasm To Vbin
 //
 //  Converts vasm to vbin.
+//	Complexity: O(n)
 //
 //
 
@@ -24,30 +25,30 @@ class Vasm {
 		int program_counter;
 		string vasm_file_name;
 		vector<vector<string>> tokens;
-		map<string, vector<string>> asm_instructions;
+		map<string, string> memory_adresses;
+		map<string, vector<string>> vasm_instructions;
 		
 		vector<vector<string>> get_tokens() 
 		{
 			return tokens;
 		}
 		
-		void print_tokens() 
+		void show_tokens() 
 		{
 			cout_tokens(tokens);
 		}
 		
-		map<string, vector<string>> get_asm_instructions() 
+		map<string, vector<string>> get_vasm_instructions() 
 		{
-			return asm_instructions;
+			return vasm_instructions;
 		}
 		
-		template<typename T>
-		string int_to_hex(T i)
+		string int_to_hex(int i)
 		{
 			stringstream stream;
 			stream << "0x" 
 				   << setfill('0') 
-				   << setw(sizeof(T)*2) 
+				   << setw(sizeof(int) * 2) 
 				   << hex 
 				   << i;
 			return stream.str();
@@ -56,36 +57,94 @@ class Vasm {
 		void generate_addresses() 
 		{
 			for (auto& tokens_vector: tokens) {
-				string first_instruction = tokens_vector[0];
-				auto search = asm_instructions.find(first_instruction);
-				// push program counter current value to tokens_vector
+				auto instruction_address = int_to_hex(program_counter);
+				// push instruction address to tokens vector
 				tokens_vector.push_back(
-					// instruction address
-					int_to_hex(program_counter)
+					instruction_address
 				);
-				// if first instruction is an asm instruction
-				if (search != asm_instructions.end()) {
-					// add asm instruction bytes to program counter
-					program_counter += stoi(asm_instructions[first_instruction][0]);
+				auto first_token = tokens_vector[0];
+				auto vasm_search = vasm_instructions.find(first_token);
+				// if first token is an vasm instruction
+				if (vasm_search != vasm_instructions.end()) {
+					// add vasm instruction bytes to program counter
+					program_counter += stoi(vasm_instructions[first_token][0]);
 				}
 			}
+		}
+		
+		void save_labels_addresses() 
+		{
+			for (auto& tokens_vector: tokens) {
+				// if tokens vector only have two tokens
+				if (tokens_vector.size() == 2) {
+					auto first_token = tokens_vector[0];
+					auto vasm_search = vasm_instructions.find(first_token);
+					// if tokens vector represents a vasm label
+					// => i.e. if first token is not a vasm instruction
+					if (vasm_search == vasm_instructions.end()) {
+						// save token address
+						auto address = tokens_vector[1];
+						memory_adresses[first_token] = address;
+					}
+				}
+			}
+		}
+		
+		string string_to_ascii_hex(string s) 
+		{
+			stringstream stream;	
+			auto first = s.begin();
+			auto last = s.end();
+			while (first != last) {
+				stream << "0x" << hex << static_cast<int>(*first) << " ";
+				++first;
+			}
+			return stream.str();
 		}
 		
 		void generate_code() 
 		{
 			for (auto& tokens_vector: tokens) {
-				string first_instruction = tokens_vector[0];
-				auto search = asm_instructions.find(first_instruction);
-				// if first instruction is an asm instruction
-				if (search != asm_instructions.end()) {
+				auto first_token = tokens_vector[0];
+				auto vasm_search = vasm_instructions.find(first_token);
+				// if first token is an vasm instruction
+				if (vasm_search != vasm_instructions.end()) {
 					// push instruction code to tokens_vector
 					tokens_vector.push_back(
-						// asm instruction code
-						asm_instructions[first_instruction][1]
+						// vasm instruction code
+						vasm_instructions[first_token][1]
 					);
-					// TODO
+				}
+				// if tokens vector only have four tokens
+				// => i.e. if is a vasm instruction 
+				if (tokens_vector.size() == 4) {
+					auto second_token = tokens_vector[1];
+					auto memory_search = memory_adresses.find(second_token);
+					// if second token is a label with a valid memory address
+					if (memory_search != memory_adresses.end()) {
+						tokens_vector.push_back(
+							memory_adresses[second_token]
+						);
+					} else {
+						tokens_vector.push_back(
+							string_to_ascii_hex(second_token)
+						);
+					}
 				}
 			}
+		}
+		
+		void save_vbin_code() 
+		{
+			ofstream vbin_code;
+			vbin_code.open("code.vbin", ofstream::out | ofstream::trunc);
+			for (auto& tokens_vector: tokens) {
+				for (auto& t: tokens_vector) {
+					vbin_code << t << " ";
+				}
+				vbin_code << endl;
+			}
+			vbin_code.close();
 		}
 		
 	public:
@@ -97,63 +156,63 @@ class Vasm {
 			tokens = tokenize_lines(file_name);
 			// Step 2.
 			/* { "load": {"4", "1"} } */
-			asm_instructions["load"].push_back("4");    // bytes
-			asm_instructions["load"].push_back("01");   // code
+			vasm_instructions["load"].push_back("4");      // bytes
+			vasm_instructions["load"].push_back("0x01");   // code
 			/* { "loadl": {"9", "2"} } */
-			asm_instructions["loadl"].push_back("9");   // bytes 
-			asm_instructions["loadl"].push_back("02");  // code
+			vasm_instructions["loadl"].push_back("9");     // bytes 
+			vasm_instructions["loadl"].push_back("0x02");  // code
 			/* { "store": {"4", "3"} } */
-			asm_instructions["store"].push_back("4");   // bytes
-			asm_instructions["store"].push_back("03");  // code
+			vasm_instructions["store"].push_back("4");     // bytes
+			vasm_instructions["store"].push_back("0x03");  // code
 			/* { "add": {"1", "4"} } */
-			asm_instructions["add"].push_back("1");     // bytes 
-			asm_instructions["add"].push_back("04");    // code
+			vasm_instructions["add"].push_back("1");       // bytes 
+			vasm_instructions["add"].push_back("0x04");    // code
 			/* { "sub": {"1", "5"} } */
-			asm_instructions["sub"].push_back("1");     // bytes
-			asm_instructions["sub"].push_back("05");    // code
+			vasm_instructions["sub"].push_back("1");       // bytes
+			vasm_instructions["sub"].push_back("0x05");    // code
 			/* { "mult": {"1", "6"} } */
-			asm_instructions["mult"].push_back("1");    // bytes
-			asm_instructions["mult"].push_back("06");   // code
+			vasm_instructions["mult"].push_back("1");      // bytes
+			vasm_instructions["mult"].push_back("0x06");   // code
 			/* { "equal": {"1", "7"} } */
-			asm_instructions["equal"].push_back("1");   // bytes
-			asm_instructions["equal"].push_back("07");  // code
+			vasm_instructions["equal"].push_back("1");     // bytes
+			vasm_instructions["equal"].push_back("0x07");  // code
 			/* { "jmp": {"4", "8"} } */
-			asm_instructions["jmp"].push_back("4");     // bytes
-			asm_instructions["jmp"].push_back("08");    // code
+			vasm_instructions["jmp"].push_back("4");       // bytes
+			vasm_instructions["jmp"].push_back("0x08");    // code
 			/* { "jmpz": {"4", "9"} } */
-			asm_instructions["jmpz"].push_back("4");    // bytes 
-			asm_instructions["jmpz"].push_back("09");   // code
+			vasm_instructions["jmpz"].push_back("4");      // bytes 
+			vasm_instructions["jmpz"].push_back("0x09");   // code
 			/* { "jmpnz": {"4" "10"} } */
-			asm_instructions["jmpnz"].push_back("4");   // bytes   
-			asm_instructions["jmpnz"].push_back("0A");  // code
+			vasm_instructions["jmpnz"].push_back("4");     // bytes   
+			vasm_instructions["jmpnz"].push_back("0x0A");  // code
 			/* { "edit": {"2", "11"} } */
-			asm_instructions["edit"].push_back("2");    // bytes
-			asm_instructions["edit"].push_back("1N");   // code
+			vasm_instructions["edit"].push_back("2");      // bytes
+			vasm_instructions["edit"].push_back("0x1N");   // code
 			/* { "print": {"1", "12"} } */
-			asm_instructions["print"].push_back("1");   // bytes 
-			asm_instructions["print"].push_back("0C");  // code
+			vasm_instructions["print"].push_back("1");     // bytes 
+			vasm_instructions["print"].push_back("0x0C");  // code
 			/* { "halt": {"1", "13"} } */
-			asm_instructions["halt"].push_back("1");    // bytes
-			asm_instructions["halt"].push_back("0D");   // code
+			vasm_instructions["halt"].push_back("1");      // bytes
+			vasm_instructions["halt"].push_back("0x0D");   // code
 			/* { "space": {"1", "14"} } */
-			asm_instructions["space"].push_back("1");   // bytes 
-			asm_instructions["space"].push_back("2N");  // code
+			vasm_instructions["space"].push_back("1");     // bytes 
+			vasm_instructions["space"].push_back("0x2N");  // code
 			/* { "block": {"1", "0"} } */
-			asm_instructions["block"].push_back("8");   // bytes  
-			asm_instructions["block"].push_back("");    // code
+			vasm_instructions["block"].push_back("8");     // bytes  
+			vasm_instructions["block"].push_back("");      // code
 			/* { "end": {"1", "0"} } */
-			asm_instructions["end"].push_back("1");     // bytes  
-			asm_instructions["end"].push_back("");      // code
+			vasm_instructions["end"].push_back("1");       // bytes  
+			vasm_instructions["end"].push_back("");        // code
 		}
 		
 		void run() 
 		{
 			generate_addresses();
+			save_labels_addresses();
 			generate_code();
-			print_tokens();
-		}
-		
-		
+			show_tokens();
+			save_vbin_code();
+		}	
 		
 };
 
